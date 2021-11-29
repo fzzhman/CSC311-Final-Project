d94cb1a8a5e259c5a93d66a5ba239b9cd388f821
jl1600 <jack.liu1600@gmail.com>
2021-11-28 22:52:40 -0500

    100% done with adaboost ensemble coding, doing debugging now

--- part_b/adaboost_ensemble.py
+++ part_b/adaboost_ensemble.py
@@ -57,9 +57,52 @@ def weighted_bagging(zero_train_matrix, weight_matrix, N):
     return  current_training_set
 
 
-def update_model_weight(model,)
+def evaluate_model_weight(model, train_data, valid_data):
+    """ Evaluate the valid_data on the current model.
+
+    :param model: Module
+    :param train_data: 2D FloatTensor
+    :param valid_data: A dictionary {user_id: list,
+    question_id: list, is_correct: list}
+    :return: float
+    """
+    # Tell PyTorch you are evaluating the model.
+    model.eval()
+
+    total = 0
+    correct = 0
+
+    got_wrong = np.zeros((len(train_data),1))
+    for i, u in enumerate(valid_data["user_id"]):
+        inputs = Variable(train_data[u]).unsqueeze(0)
+        output = model(inputs)
+
+        guess = output[0][valid_data["question_id"][i]].item() >= 0.5
+        if guess == valid_data["is_correct"][i]:
+            correct += 1
+        else:
+            got_wrong[u] += 1
+        total += 1
+    acc = correct / float(total)
+    model_weight = 0.5 * np.log(acc / (1 - acc))
+
+    return model_weight,got_wrong
+
+def update_sample_weight(sample_weight_matrix,wrong_samples, model_weight):
+    updated_weight_sum = 0
+    for item_index in range(len(wrong_samples)):
+
+        if wrong_samples[item_index] > 0: # samples that got wrong need more focus, increase weight
+            sample_weight_matrix[item_index] = sample_weight_matrix[item_index] * (np.e ** model_weight)
+        elif wrong_samples[item_index] <= 0: #samples that got correct can predict easily, decrease weight
+            sample_weight_matrix[item_index] = sample_weight_matrix[item_index] * (np.e ** -model_weight)
+
+        updated_weight_sum += sample_weight_matrix[item_index]
+    sample_weight_matrix = sample_weight_matrix / updated_weight_sum
 
 def evaluate_adaboost_ensemble():
+
+    # set initial parameters, load data
     zero_train_matrix, train_matrix, valid_data, test_data = load_data()
 
     N = len(zero_train_matrix)
@@ -74,11 +117,39 @@ def evaluate_adaboost_ensemble():
 
         current_training_set = torch.FloatTensor(current_training_set)
         train_matrix = torch.FloatTensor(train_matrix)
+        tensor_zero_train_matrix = torch.FloatTensor(zero_train_matrix)
+        model = 0
         # train
         if model_index == 0:
         # train knn
+            model = nn.AutoEncoder(num_question=len(train_matrix[0]), k=11, p=0)
+            print("training neural net")
+            print("k* = " + str(10) + "; learning rate = " + str(0.05)
+                  + "; num_epoch = " + str(20) + "; lamb=" + str(0.00025) + "; p=" + str(0))
+            nn.train(model,
+                     lr=0.05,
+                     lamb=0.00025,
+                     train_data=train_matrix,
+                     zero_train_data=current_training_set,
+                     valid_data=valid_data,
+                     num_epoch=20)
+            test_acc = nn.evaluate(model, current_training_set, test_data)
+            print("Test Acc: {}".format(test_acc))
         elif model_index == 1:
         # train IRT
+            model = nn.AutoEncoder(num_question=len(train_matrix[0]), k=12, p=0)
+            print("training neural net")
+            print("k* = " + str(10) + "; learning rate = " + str(0.05)
+                  + "; num_epoch = " + str(20) + "; lamb=" + str(0.00025) + "; p=" + str(0))
+            nn.train(model,
+                     lr=0.05,
+                     lamb=0.00025,
+                     train_data=train_matrix,
+                     zero_train_data=current_training_set,
+                     valid_data=valid_data,
+                     num_epoch=20)
+            test_acc = nn.evaluate(model, current_training_set, test_data)
+            print("Test Acc: {}".format(test_acc))
         elif model_index == 2:
         # train neural net
             model = nn.AutoEncoder(num_question=len(train_matrix[0]), k=10, p=0)
@@ -96,55 +167,11 @@ def evaluate_adaboost_ensemble():
             print("Test Acc: {}".format(test_acc))
 
         # update model weight
-        model_weights[model_index] = update_model_weight()
-
-
-
-
-
-    # eva
-    # update model weight
-    # update sample weight
-
-    knn_base_dist = np.random.randint(0, len(train_matrix))
-    nn_base_dist = np.random.randint(0, len(train_matrix))
-    # TODO: Implement IRT
-
-    # Train
-    nn_model = train_neural_network(zero_train_matrix[nn_base_dist], train_matrix)
-    # TODO: Implement IRT
-
-    # Predict
-    knn_correct = 0
-    nn_correct = 0
-    correct = 0
-    total = 0
-    for i, u in enumerate(valid_data["user_id"]):
-        q_id = valid_data["question_id"][i]
-
-        knn_pred = predict_knn(train_matrix[u], q_id, train_matrix)
-        nn_pred = predict_nn(zero_train_matrix[u], q_id, nn_model)
-        # TODO: Implement IRT
-
-        avg = (knn_pred + nn_pred) / 2
-        # TODO: Implement IRT
-
-        actual = bool(valid_data["is_correct"][i])
-        if (avg >= 0.5) == actual:
-            correct += 1
-            total += 1
-        if (knn_pred >= 0.5) == actual:
-            knn_correct += 1
-        if (nn_pred >= 0.5) == actual:
-            nn_correct += 1
-        print("Iteration: {}, Current correct: {}, Current kNN correct: {}, Current NN correct: {}".format(i, correct,
-                                                                                                           knn_correct,
-                                                                                                           nn_correct))
-    return {
-        "ensemble_acc": correct / float(total),
-        "knn_bagged_acc": knn_correct / float(total),
-        "nn_bagged_acc": nn_correct / float(total)
-    }
+        model_weights[model_index],wrong_samples = evaluate_model_weight(model,
+                                                                        current_training_set,
+                                                                        valid_data=valid_data)
+        # update sample weight
+        sample_weight_matrix = update_sample_weight(sample_weight_matrix,wrong_samples,model_weights[model_index])
 
 
 def main():
